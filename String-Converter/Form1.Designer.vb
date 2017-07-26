6c25c5745d20f3fcc0b2ec7b3f16cd1fd654eee8
pozzum <pozzumspityt@gmail.com>
2017-07-25 22:34:54 -0400

    Commiting actual files

---
+++ String-Converter/Form1.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenCSVDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.SavePACHDialog = New System.Windows.Forms.SaveFileDialog()
+        Me.Num_String_Count = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Num_Max_Length = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Num_Start_Num = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SaveCSVDialog = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenPACHDialog = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.Num_String_Count, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Num_Max_Length, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Num_Start_Num, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(84, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Build Pach"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenCSVDialog
+        '
+        Me.OpenCSVDialog.FileName = "*.csv"
+        Me.OpenCSVDialog.Filter = "Comma Seperated Values|*.csv|All Files|*.*"
+        '
+        'SavePACHDialog
+        '
+        Me.SavePACHDialog.FileName = "0001.pach"
+        Me.SavePACHDialog.Filter = "Pach File|*.pac;*.pach;*.dat|All Files|*.*"
+        '
+        'Num_String_Count
+        '
+        Me.Num_String_Count.Location = New System.Drawing.Point(13, 42)
+        Me.Num_String_Count.Maximum = New Decimal(New Integer() {65535, 0, 0, 0})
+        Me.Num_String_Count.Name = "Num_String_Count"
+        Me.Num_String_Count.Size = New System.Drawing.Size(83, 20)
+        Me.Num_String_Count.TabIndex = 2
+        Me.Num_String_Count.Value = New Decimal(New Integer() {4249, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(102, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "New Strings"
+        '
+        'Num_Max_Length
+        '
+        Me.Num_Max_Length.Location = New System.Drawing.Point(13, 69)
+        Me.Num_Max_Length.Name = "Num_Max_Length"
+        Me.Num_Max_Length.Size = New System.Drawing.Size(83, 20)
+        Me.Num_Max_Length.TabIndex = 4
+        Me.Num_Max_Length.Value = New Decimal(New Integer() {30, 0, 0, 0})
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(102, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "String Length"
+        '
+        'Num_Start_Num
+        '
+        Me.Num_Start_Num.Location = New System.Drawing.Point(13, 95)
+        Me.Num_Start_Num.Maximum = New Decimal(New Integer() {65535, 0, 0, 0})
+        Me.Num_Start_Num.Name = "Num_Start_Num"
+        Me.Num_Start_Num.Size = New System.Drawing.Size(83, 20)
+        Me.Num_Start_Num.TabIndex = 6
+        Me.Num_Start_Num.Value = New Decimal(New Integer() {13734, 0, 0, 0})
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(102, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "First New String"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(13, 121)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(83, 20)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.Text = "35A6"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(102, 124)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(90, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Hex of First String"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(102, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Extract csv"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(13, 147)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(83, 20)
+        Me.TextBox2.TabIndex = 11
+        Me.TextBox2.Text = "4640"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(102, 150)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(91, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Hex of Last String"
+        '
+        'SaveCSVDialog
+        '
+        Me.SaveCSVDialog.FileName = "*.csv"
+        Me.SaveCSVDialog.Filter = "Comma Seperated Values|*.csv|All Files|*.*"
+        '
+        'OpenPACHDialog
+        '
+        Me.OpenPACHDialog.FileName = "0001.pach"
+        Me.OpenPACHDialog.Filter = "Pach File|*.pac;*.pach;*.dat|All Files|*.*"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(208, 181)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Num_Start_Num)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Num_Max_Length)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Num_String_Count)
+        Me.Controls.Add(Me.Button2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "String Converter"
+        CType(Me.Num_String_Count, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Num_Max_Length, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Num_Start_Num, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenCSVDialog As OpenFileDialog
+    Friend WithEvents SavePACHDialog As SaveFileDialog
+    Friend WithEvents Num_String_Count As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Num_Max_Length As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Num_Start_Num As NumericUpDown
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents SaveCSVDialog As SaveFileDialog
+    Friend WithEvents OpenPACHDialog As OpenFileDialog
+End Class
---
+++ String-Converter/Form1.vb
@@ -0,0 +1,164 @@
+﻿Imports System
+Imports System.IO
+Imports System.IO.Stream
+Imports Microsoft.VisualBasic
+Imports Microsoft.VisualBasic.FileIO
+Public Class Form1
+    Dim counter As Integer = 0
+
+    Function Bytes2Dec(array() As Byte)
+        Dim Number As Int32 = array(3) * 256 * 256 * 256 + array(2) * 256 * 256 + array(1) * 256 + array(0)
+        'MessageBox.Show(Number.ToString & "Count" & counter)
+        Return Number
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If OpenCSVDialog.ShowDialog = System.Windows.Forms.DialogResult.OK Then
+            If SavePACHDialog.ShowDialog = System.Windows.Forms.DialogResult.OK Then
+
+                counter = 0
+                'First How many new strings
+                Dim NewStringCount As Integer = Num_String_Count.Value
+                'How Big Are the strings
+                Dim stringsize As Integer = Num_Max_Length.Value
+                Dim newdataset(NewStringCount * 12 + NewStringCount * stringsize + 8 - 1) As Byte
+                '12 to say string attirbutes then the string length
+                Dim NewStart(3) As Byte 'a byteset for the location in the file
+                'now to set the string reference
+                Dim StringName(3) As Byte
+                Dim StartString As Int32 = Num_Start_Num.Value ' 35A6 first new string to be 35A7
+                StringName = BitConverter.GetBytes(StartString)
+                'we need the total string count for the start
+                Dim Stringcount(3) As Byte
+                'we need the first strings location - string length
+                NewStart = BitConverter.GetBytes(8 + NewStringCount * 12 - stringsize)
+                'now to build the file
+                For i As Integer = 0 To 3
+                    newdataset(i) = 0
+                Next
+                Stringcount = BitConverter.GetBytes(NewStringCount)
+                newdataset(4) = Stringcount(0)
+                newdataset(5) = Stringcount(1)
+                newdataset(6) = Stringcount(2)
+                newdataset(7) = Stringcount(3)
+                For i As Integer = 8 To 7 + NewStringCount * 12
+                    If 0 <= counter AndAlso counter < 3 Then
+                        counter = counter + 1
+                    ElseIf counter = 3 Then 'Setting String Location
+                        NewStart = BitConverter.GetBytes(Bytes2Dec(NewStart) + stringsize)
+                        newdataset(i - 3) = NewStart(0)
+                        newdataset(i - 2) = NewStart(1)
+                        newdataset(i - 1) = NewStart(2)
+                        newdataset(i) = NewStart(3)
+                        counter = counter + 1
+                    ElseIf 3 < counter AndAlso counter < 7 Then
+                        counter = counter + 1
+                    ElseIf counter = 7 Then 'Setting String Length
+                        Dim temp() As Byte = BitConverter.GetBytes(stringsize)
+                        newdataset(i - 3) = temp(0)
+                        newdataset(i - 2) = temp(1)
+                        newdataset(i - 1) = temp(2)
+                        newdataset(i) = temp(3)
+                        counter = counter + 1
+                    ElseIf 7 < counter AndAlso counter < 11 Then
+                        counter = counter + 1
+                    ElseIf counter = 11 Then
+                        StringName = BitConverter.GetBytes(Bytes2Dec(StringName) + 1)
+                        newdataset(i - 3) = StringName(0)
+                        newdataset(i - 2) = StringName(1)
+                        newdataset(i - 1) = StringName(2)
+                        newdataset(i) = StringName(3)
+                        counter = 0
+                    End If
+                Next
+                Dim StartByte As Integer = 8 + NewStringCount * 12
+                Dim TextArray() As String ' NewStringCount - 1
+
+                Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(OpenCSVDialog.FileName)
+                    MyReader.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited
+                    MyReader.Delimiters = New String() {","}
+                    Dim counter As Integer = 0
+                    Do While Not MyReader.EndOfData
+                        Try
+                            TextArray = MyReader.ReadFields
+                            Dim bytearray() As Byte
+                            'MessageBox.Show(TextArray.Length)
+
+                            Try
+                                bytearray = System.Text.Encoding.ASCII.GetBytes(TextArray(0))
+                            Catch ex As Exception
+                                MessageBox.Show(ex.Message & counter & TextArray(0)) '& TextArray(i) 
+                            End Try
+                            For j As Integer = 0 To 29
+                                Try
+                                    If j < bytearray.Length Then
+                                        newdataset(StartByte + counter * 30 + j) = bytearray(j)
+                                    End If
+                                Catch ex As Exception
+                                    MessageBox.Show("Error: " & counter & " " & TextArray(0) & " " & j & " " & Mid(TextArray(0), j - 1, j))
+                                End Try
+                            Next
+                        Catch ex As FileIO.MalformedLineException
+                            Stop
+                        End Try
+                        counter = counter + 1
+                    Loop
+                    'For i As Integer = 0 To NewStringCount - 1
+                    'Try
+                    'TextArray = MyReader.ReadFields
+                    'My.Computer.FileSystem.WriteAllText(
+                    '"C:\Users\Pozzum\Desktop\Book2.csv", TextArray(i), True) 'Book1.csv
+                    ' Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
+                    'MsgBox("Line " & ex.Message & " is invalid.  Skipping")
+                    '
+                    'End Try
+                    'Next
+                End Using
+
+                Try
+                    File.WriteAllBytes(SavePACHDialog.FileName, newdataset)
+                Catch ex As Exception
+                    'MessageBox.Show(ex.Message)
+                End Try
+                MessageBox.Show("String Built")
+            End If
+        End If
+
+    End Sub
+
+    Private Sub NumericUpDown3_ValueChanged(sender As Object, e As EventArgs) Handles Num_Start_Num.ValueChanged
+        TextBox1.Text = Hex(Num_Start_Num.Value)
+        TextBox2.Text = Hex(Num_Start_Num.Value + Num_String_Count.Value)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If OpenPACHDialog.ShowDialog = System.Windows.Forms.DialogResult.OK Then
+            If SaveCSVDialog.ShowDialog = System.Windows.Forms.DialogResult.OK Then
+                Dim Source As Byte() = File.ReadAllBytes(OpenPACHDialog.FileName)
+                Dim String_Count As Integer = Bytes2Dec({Source(4), Source(5), Source(6), Source(7)})
+                Num_String_Count.Value = String_Count
+                Dim Max_Length As Integer = 0
+                Dim String_List(String_Count - 1) As String
+                Dim Ref_Num(String_Count - 1) As Integer
+                For i As Integer = 0 To String_Count - 1
+                    Ref_Num(i) = Bytes2Dec({Source(&H10 + 12 * i), Source(&H11 + 12 * i), Source(&H12 + 12 * i), Source(&H13 + 12 * i)})
+                    Dim Start_Num As Integer = Bytes2Dec({Source(&H8 + 12 * i), Source(&H9 + 12 * i), Source(&HA + 12 * i), Source(&HB + 12 * i)})
+                    Dim Length As Integer = Bytes2Dec({Source(&HC + 12 * i), Source(&HD + 12 * i), Source(&HE + 12 * i), Source(&HF + 12 * i)})
+                    If Length > Max_Length Then
+                        Max_Length = Length
+                    End If
+                    Dim String_Bytes As Byte() = New Byte(Length - 1) {}
+                    Buffer.BlockCopy(Source, Start_Num, String_Bytes, 0, Length)
+                    String_List(i) = System.Text.Encoding.ASCII.GetString(String_Bytes)
+                Next
+                Dim CSV_Writer As System.IO.StreamWriter
+                CSV_Writer = My.Computer.FileSystem.OpenTextFileWriter(SaveCSVDialog.FileName, False)
+                For i As Integer = 0 To String_Count - 1
+                    CSV_Writer.WriteLine(String_List(i) & "," & Hex(Ref_Num(i)))
+                Next
+                CSV_Writer.Close()
+                MessageBox.Show("Writing Complete")
+            End If
+        End If
+    End Sub
+End Class
